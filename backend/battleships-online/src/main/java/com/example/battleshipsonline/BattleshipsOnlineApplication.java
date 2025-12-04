dc240bd05fa6319e3319659ff3baa958e93ba447
SlowToes <yongdetoh@gmail.com>
2025-12-04 17:58:04 +0800

    Initial Commit

---
+++ backend/battleships-online/src/main/java/com/example/battleshipsonline/BattleshipsOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.battleshipsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipsOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleshipsOnlineApplication.class, args);
+    }
+
+}
---
+++ backend/battleships-online/src/test/java/com/example/battleshipsonline/BattleshipsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.battleshipsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipsOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
